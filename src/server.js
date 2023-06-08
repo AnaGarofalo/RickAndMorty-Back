c7c3d3f554b5ce13704f5bba15b10716a88bb842
Ana María Garófalo <119868532+AnaGarofalo@users.noreply.github.com>
2023-06-08 19:32:53 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,18 +7,8 @@ require("dotenv").config();
 const PORT = 3001;
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
 app.use("/rickandmorty", router);
 
 database.sync({ alter: true }).then(() => {
